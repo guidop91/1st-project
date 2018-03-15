7fb27eb6fee6484b999b6266a905105dac70086c
Guido Perdomo <guidop91@gmail.com>
2018-03-15 11:50:57 -0400

    Add a few comments

--- casino_game.py
+++ casino_game.py
@@ -6,10 +6,10 @@ class Deck:
 		self.cards = []
 
 		self.populate()
-		shuffle(self.cards)
+		shuffle(self.cards) #Shuffles cards
 
 	def populate(self):
-
+		#Creates deck of cards
 		symbol = ["Spade","Diamond","Heart","Club"]
 		number = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"]
 
@@ -29,4 +29,4 @@ class Table():
 	def __init__(self):
 
 		self.in_game = []
-		self.build = []
+		self.build = [] #Shows builds for cards in play
