2d8e1f9cf9ec9cb2defbfeab87263ef28a427606
Guido Perdomo <guidop91@gmail.com>
2018-03-21 19:44:26 -0400

    Add class for Cards

--- casino_game.py
+++ casino_game.py
@@ -1,5 +1,13 @@
 from random import shuffle
 
+class Card:
+	SUIT = ["Spade","Diamond","Heart","Club"]
+	RANK = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"]
+
+	def __init__(self,suit,rank):
+		self.suit = suit
+		self.rank = rank
+
 class Deck:
 	def __init__(self):
 		
