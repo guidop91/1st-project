bb4f94398d90921a726f84267cd64b842d7e1ed9
Guido Perdomo <guidop91@gmail.com>
2018-03-21 13:07:29 -0400

    Complete player function build()

--- casino_game.py
+++ casino_game.py
@@ -60,12 +60,16 @@ class Player():
 		self.has_build = False
 
 	def build(self,table):
-		central_card = None
-		central_cardv = None
-		result = []
-		resultv = [] #The v suffix stands for value: Only the value of the card is taken.
 		#Entire process
 		while True:
+			start_over = False
+			central_card = None
+			central_cardv = None
+			result = []
+			resultv = [] #The v suffix stands for value: Only the value of the card is taken.
+			quit = False
+
+			print("You have entered the build function. If at any time you want to get out, enter 'q'.")
 			#Card to build for
 			while True:
 				print(self.hand)
@@ -74,6 +78,9 @@ class Player():
 				#Safeguards
 				if not selection:
 					continue
+				if selection.lower() == 'q':
+					quit = True
+					break
 				selection = int(selection)
 				if selection > len(self.hand):
 					continue
@@ -86,12 +93,18 @@ class Player():
 
 			#Card to play to construct build.
 			while True:
+				if quit:
+					break
+
 				print(self.hand)
 				selection = input("Enter (1-%d) the card you want to build with: " % len(self.hand))
 
 				#Safeguards
 				if not selection:
 					continue
+				if selection.lower() == 'q':
+					quit = True
+					break
 				selection = int(selection)
 				if selection > len(self.hand):
 					continue
@@ -101,18 +114,94 @@ class Player():
 					continue
 
 				build_cardv = Deck().card_values(build_card[1])
+
+				if build_cardv >= central_cardv:
+					print ("Cannot use card with same or more value than the one you are building for.")
+					continue
+
 				print("You have selected ", build_card)
 				result.append(build_card)
 				resultv.append(build_cardv)
 
 				break
 
+			#Choose card from table to build with
+			while True:
+				if quit:
+					break
 
+				build_sum = 0
+				for i in resultv:
+					build_sum += i
 
+				while True:
+					if build_sum == central_cardv:
+						finish = input("You have a valid build! Do you want to choose it? Enter \"y\" for YES, \"n\" for NO. ")
 
+						if not finish:
+							continue
 
+						if finish.lower() == 'y':
+							print("Your build is ", result)
+							self.has_build = True
+							return result, central_card
 
+						if selection.lower() == 'q':
+							quit = True
+							break
 
+						if finish.lower() == 'n':
+							while True:
+								start_over = input("Would you like to start the build from scratch? Enter \"y\" for YES, \"n\" for NO. ")
+								if not start_over:
+									continue
+
+								if start_over.lower() == 'y':
+									start_over = True
+									break
+
+								if selection.lower() == 'q':
+									quit = True
+									break
+
+								else:
+									continue
+					
+					if start_over == True:
+						break
+
+					print("Your build card is: ",  central_card)
+					print(table.in_game)
+					print("Current build: %s. The added value is: %s" % (result,build_sum))
+					selection = input("Select a card (1-%d) from the table to build with. " % len(table.in_game))
+					if not selection:
+						continue
+					selection = int(selection)
+					if selection > len(table.in_game):
+						continue
+
+					build_card = table.in_game[selection-1]
+					build_cardv = Deck().card_values(build_card[1])
+
+					if build_card in result:
+						print("Cannot use card more than one time.")
+						continue
+
+					if build_sum + build_cardv > central_cardv:
+						print("This card makes the build surpass the build card.")
+						continue
+
+					result.append(build_card)
+					resultv.append(build_cardv)
+					break
+
+					
+				if start_over == True:
+					break
+				if quit:
+					break
+			if quit:
+					break
 
 class Table():
 	def __init__(self):
